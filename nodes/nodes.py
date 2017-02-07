ae27add98a529a385601e56f596c65ea310136d7
Jack <dongtianzheng@gmail.com>
2017-02-07 14:46:31 -0500

    inital commit

---
+++ nodes/nodes.py
@@ -0,0 +1,12 @@
+
+
+class Program(object):
+    def __init__(self, classes):
+        self.__classes = classes
+
+
+class Class(object):
+    def __init__(self, classType, features):
+        self.__classType = classType
+        self.__features = features
+
